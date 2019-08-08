c419719f9c77a468224a8458202a80b38e0c4cdb
rimduhui <>
2019-08-08 17:31:08 +0900

    apply DrawView to sample app

--- app/src/main/java/com/skb/duhui/socketdrawviewsample/MainActivity.kt
+++ app/src/main/java/com/skb/duhui/socketdrawviewsample/MainActivity.kt
@@ -2,11 +2,22 @@ package com.skb.duhui.socketdrawviewsample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import com.skb.duhui.socketdrawview.DrawView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var drawView: DrawView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawView = findViewById(R.id.draw_view)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        drawView.draw(event!!)
+        return super.onTouchEvent(event)
     }
 }
